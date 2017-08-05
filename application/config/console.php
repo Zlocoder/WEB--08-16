22d8f1d5d173f73e389d15d902076ac1d0d6e5dd
George Lemish <yorgus@yandex.ru>
2017-08-05 17:48:18 +0300

    first commit

---
+++ application/config/console.php
@@ -0,0 +1,51 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+$db = require(__DIR__ . '/db.php');
+
+$config = [
+    'id' => 'basic-console',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'app\commands',
+    'controllerMap' => [
+        'migrate' => [
+            'class' => 'yii\console\controllers\MigrateController',
+            'migrationTable' => '_migration',
+            'interactive' => false,
+            'migrationPath' => dirname(dirname(__DIR__)) . '/migrations'
+        ]
+    ],
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+    ],
+    'params' => $params,
+    /*
+    'controllerMap' => [
+        'fixture' => [ // Fixture generation command line.
+            'class' => 'yii\faker\FixtureController',
+        ],
+    ],
+    */
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
